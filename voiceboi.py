8f7b508215821f7b9fb46abfbf8fa0ae960e5797
RefinedDev <aryantatee@gmail.com>
2021-06-11 22:10:12 +0530

    Update

--- voiceboi.py
+++ voiceboi.py
@@ -9,6 +9,7 @@ class Access():
         self.Srmain = main
         self.Mic = mic
         self.lang = lang
+        self.WikiCheckList = ['who is','what is']
 
     def start(self):
         with self.Mic as source:
@@ -28,20 +29,16 @@ class Access():
                     speech = gTTS(text=f'Current time is {formattedtime}.', lang=self.lang,slow=False)
                     speech.save('Time.mp3')
                     playsound.playsound('Time.mp3')
-                elif 'what is' in str.lower(result):
+                elif any(value in str.lower(result) for value in self.WikiCheckList):
                     res = wikipedia.summary(result,sentences = 2)
                     speech = gTTS(text=f'According to wikipedia,{res}', lang=self.lang,slow=False)
-                    speech.save('wiki.mp3')
-                    playsound.playsound('wiki.mp3')
-                elif 'who is' in str.lower(result):
-                    res = wikipedia.summary(result,sentences = 2)
-                    speech = gTTS(text=res, lang=self.lang,slow=False)
                     speech.save('wiki.mp3')
                     playsound.playsound('wiki.mp3')
                 else:
                     speech = gTTS(text=f"Sorry, i didn't get that.", lang=self.lang,slow=False)
                     speech.save('Unrecognized.mp3')
                     playsound.playsound('Unrecognized.mp3')
+                    
             except SR.UnknownValueError as e:
                 print(e)
                 speech = gTTS(text=f"An error occured, please try again.", lang=self.lang,slow=False)
