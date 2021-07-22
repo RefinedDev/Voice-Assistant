287c5f1b398ab1c01cad3979411db2ed2a09f8b7
RefinedDev <aryantatee@gmail.com>
2021-07-22 09:11:27 +0530

    added bat file for ease

---
+++ installDepends.bat
@@ -0,0 +1,9 @@
+pip install SpeechRecognition
+pip install playsound
+pip install wikipedia
+pip install gTTS
+pip install pyweatherbit
+pip install numpy
+@ECHO OFF
+ECHO All dependancies installed.
+PAUSE
